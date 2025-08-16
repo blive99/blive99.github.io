45fe045fd584d1c10325c181b5ab8e065e2f49c8
blive99 <kwybadboy@gmail.com>
2025-08-16 09:40:21 +0700

    Update goto.js

--- goto.js
+++ goto.js
@@ -39,6 +39,7 @@ https://keela3.com/stream/41/index.m3u8?token=J5iyZmyDDiw7NdQm
 #EXTINF:-1 tvg-id="" tvg-logo="https://www.img03.xyz/assets/img/ch_logo/Monomax_Sport1.png" group-title="📺 VIP SPORT",MONOMAX1
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 7.1.2; TV BOX Build/NHG47L) AppleWebKit/537.36 (KHTML เช่น Gecko) Chrome/56.0.2924.87 Safari/537.36
 #EXTVLCOPT:http-referrer=https://99dooball.com/
+http://cdn1.googlecdn.live:8081/ballsod24hrs/monomax1/playlist.m3u8
 https://cdn2.aws-live-streaming.com/lsmscore/monomax1/chunks.m3u8
 https://stream2.aws-live-streaming.com/bbs/monomax1/playlist.m3u8
 https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax1/playlist.m3u8
@@ -50,6 +51,7 @@ https://fw4free.inwstream.com/fw4free/ch1.stream/chunklist.m3u8
 #EXTVLCOPT:http-referrer=https://99dooball.com/
 https://stream2.aws-live-streaming.com/bbs/ais-monomax-2/playlist.m3u8
 https://cdn2.aws-live-streaming.com/lsmscore/monomax2/chunks.m3u8
+https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax-2/playlist.m3u8
 https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/ais-monomax-2/playlist.m3u8
 https://fw4free.inwstream.com/fw4free/ch2.stream/chunklist.m3u8
 https://219.livedoomovies.com:4431/Monomax_Live_2HD/chunklist.m3u8
@@ -58,6 +60,7 @@ https://219.livedoomovies.com:4431/Monomax_Live_2HD/chunklist.m3u8
 #EXTVLCOPT:http-referrer=https://fw4free.com/
 https://stream2.aws-live-streaming.com/bbs/ais-monomax-3/playlist.m3u8
 https://fw4free.inwstream.com/fw4free/ch3.stream/chunklist.m3u8
+https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax-3/playlist.m3u8
 https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/ais-monomax-3/playlist.m3u8
 https://cdn2.aws-live-streaming.com/lsmscore/monomax3/chunks.m3u8
 https://sport.livedoomovies.com:4432/Monomax_Live_3HD/chunklist.m3u8
@@ -67,6 +70,7 @@ https://sport.livedoomovies.com:4432/Monomax_Live_3HD/chunklist.m3u8
 #EXTVLCOPT:http-referrer=https://99dooball.com/
 https://stream2.aws-live-streaming.com/bbs/ais-monomax-4/playlist.m3u8
 https://fw4free.inwstream.com/fw4free/ch4.stream/chunklist.m3u8
+https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax-4/playlist.m3u8
 https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/ais-monomax-4/playlist.m3u8
 https://cdn2.aws-live-streaming.com/lsmscore/monomax4/chunks.m3u8
 https://sport.livedoomovies.com:4432/Monomax_Live_4HD/chunklist.m3u8
@@ -77,11 +81,13 @@ https://sport.livedoomovies.com:4432/Monomax_Live_4HD/chunklist.m3u8
 https://fw4free.inwstream.com/fw4free/ch5.stream/chunklist.m3u8
 https://cdn2.aws-live-streaming.com/lsmscore/monomax5/chunks.m3u8
 https://sport.livedoomovies.com:4432/Monomax_Live_5HD/chunklist.m3u8
+https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax-5/playlist.m3u8
 https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/ais-monomax-5/playlist.m3u8
 #EXTINF:-1 tvg-id="" tvg-logo="https://www.img03.xyz/assets/img/ch_logo/Monomax_Sport6.png" group-title="📺 VIP SPORT",MONOMAX6
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 7.1.2; TV BOX Build/NHG47L) AppleWebKit/537.36 (KHTML เช่น Gecko) Chrome/56.0.2924.87 Safari/537.36
 #EXTVLCOPT:http-referrer=https://www.movewinhd.com/%E0%B8%97%E0%B8%B5%E0%B8%A7%E0%B8%B5
 https://sport.livedoomovies.com:4432/Monomax_Live_6HD/chunklist.m3u8
+https://dkl-c33es.cdn.akamaiz.com/dooballfree24hr/monomax-6/playlist.m3u8
 
 #EXTINF:-1 tvg-id="" tvg-name="MONOMAX 7" tvg-logo="http://103.114.203.129:8080/images/1e027b07c0170facd7cd7b396caee3eb.png" group-title="📺 VIP SPORT",MONOMAX 7
 http://playid.ddnsgeek.com:8080/thaihd/12345/51187
