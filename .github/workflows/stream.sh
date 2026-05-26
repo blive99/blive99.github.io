62f864fcc7f97cb2c447c98af3a0f7c92f8f453a
blive99 <kwybadboy@gmail.com>
2026-05-26 12:14:05 +0700

    Update stream.sh

--- .github/workflows/stream.sh
+++ .github/workflows/stream.sh
@@ -2,24 +2,20 @@
 
 while true
 do
-    echo "Starting stream..."
+  echo "Starting FFmpeg..."
 
-    ffmpeg \
-    -loglevel error \
-    -reconnect 1 \
-    -reconnect_streamed 1 \
-    -reconnect_at_eof 1 \
-    -reconnect_delay_max 10 \
-    -rw_timeout 15000000 \
-    -user_agent "Mozilla/5.0" \
-    -i "https://dkl-1.shop/dookeela/truefilm2/chunks.m3u8" \
-    -c copy \
-    -f hls \
--hls_time 5 \
--hls_list_size 10 \
--hls_flags delete_segments+append_list \
-output.m3u8
+  ffmpeg \
+  -headers $'Referer: https://dookeela.live/\r\nUser-Agent: Mozilla/5.0\r\n' \
+  -reconnect 1 \
+  -reconnect_streamed 1 \
+  -reconnect_at_eof 1 \
+  -reconnect_delay_max 10 \
+  -rw_timeout 30000000 \
+  -timeout 30000000 \
+  -i "https://dkl-1.shop/dookeela/truefilm2/chunks.m3u8" \
+  -c copy \
+  -f null -
 
-    echo "Stream disconnected! Reconnecting in 5 seconds..."
-    sleep 5
+  echo "FFmpeg crashed. Restarting in 5 seconds..."
+  sleep 5
 done
