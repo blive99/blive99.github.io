2273be249647541163451d1e3880ffdbb60f4395
blive99 <kwybadboy@gmail.com>
2026-09-19 11:41:54 +0700

    Update check_m3u8.py

--- check_m3u8.py
+++ check_m3u8.py
@@ -3,8 +3,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 BASE_URL = "https://apitv.moneyclub999.com/ngz168/{}/playlist.m3u8"
 
-START = 1
-END = 100
+START = 100
+END = 200
 
 OUTPUT_FILE = "working_links.txt"
 
